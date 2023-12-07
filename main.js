d65c0b60f236b43ef88bccc2d2c11600d87fece5
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-12-08 01:09:43 +0600

    add more feature

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ window.onload = () => {
 function main(){
     const root = document.querySelector('.Contaienr');
     const btn = document.querySelector('#handleButton');
+    const inpVal = document.querySelector('#Selector');
     btn.addEventListener('click', function(){
         const genColor = genarateColor();
         root.style.backgroundColor = genColor;
+        inpVal.value = genColor;
     })
 }
 
@@ -17,5 +19,5 @@ function genarateColor(){
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
 
-    return `rgba(${red}, ${green}, ${blue})`
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
 }
