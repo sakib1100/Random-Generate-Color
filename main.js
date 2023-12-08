d54d7142848b9d302877bcf32f0f6b506ba4bb24
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-12-08 16:59:36 +0600

    copy color

--- main.js
+++ main.js
@@ -7,12 +7,26 @@ function main(){
     const root = document.querySelector('.Contaienr');
     const btn = document.querySelector('#handleButton');
     const inpVal = document.querySelector('#Selector');
+    const copyBtn = document.querySelector('#copyBtn');
+    const copyed = document.querySelector('#copyed');
+    copyed.style.display = 'none';
+
     btn.addEventListener('click', function(){
         const genColor = genarateColor();
         root.style.backgroundColor = genColor;
         inpVal.value = genColor;
     })
-}
+    copyBtn.addEventListener('click',function(){
+        navigator.clipboard.writeText(inpVal.value);
+        copyed.style.display = 'block';
+    setTimeout(function(){
+        copyed.style.display = 'none';
+    },2000)
+
+
+    })
+    
+}  
 
 function genarateColor(){
     const red = Math.floor(Math.random() * 255);
